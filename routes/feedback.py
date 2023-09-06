551dd8ff5204c95f3ba5d8960f17fed58de50815
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2023-09-06 22:01:08 +0800

    feedback

--- routes/feedback.py
+++ routes/feedback.py
@@ -8,26 +8,30 @@ from flask import flash, Blueprint, render_template, request, session, redirect,
 from flask_mail import Message
 
 import app
-from classes.User import User
+from classes.Feedback import Feedback
 from forms import feedbackForm
 from functions import flashFormErrors, goBack, loginAccess
 
 
 feedback = Blueprint("feedback", __name__)
 
-@feedback.route('/feedback/feedback', methods=['GET', 'POST'])
+@feedback.route('/feedback/feedback/<id>', methods=['GET', 'POST'])
 @loginAccess
-def feedbacks():
-    print("here")
+def feedbacks(id):
+    
     form = feedbackForm(request.form)
     if request.method == "POST" and form.validate():
         userDict = {}
         userDict = session["user"] 
-        email = userDict.email
+        useremail = userDict['email']
+        q1 = form.q1.data
+        q2 = form.q2.data
         experience_details = form.experience_details.data
         comments = form.comments.data
+        Feedback(useremail,id,q1,q2,experience_details,comments)
 
         with shelve.open("feedbacks") as feedback:
-            with shelve.open("users") as users:
-                pass
+            feedback["feedback"] = Feedback
+            flash("Feedback successfully sent!", category="success")
+
     return render_template("feedback/feedback.html", form=form)
